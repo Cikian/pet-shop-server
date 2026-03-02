62420dec9f1965951d6d819b3d69c05c7273d85f
Cikian_Y9000P <cikian@126.com>
2026-03-02 17:05:38 +0800

    feat(产品): 添加商品相关VO类
    添加SkuVo、SpecVo和Product4Detail三个VO类，用于商品模块的数据封装

---
+++ src/main/java/cn/cikian/shop/product/entity/vo/SkuVo.java
@@ -0,0 +1,12 @@
+package cn.cikian.shop.product.entity.vo;
+
+
+/**
+ * @author Cikian
+ * @since  2026-03-02 10:50
+ * @version 1.0
+ * @see <a href="https://www.cikian.cn">https://www.cikian.cn</a>
+ * @implNote 
+ */
+public class SkuVo {
+}
