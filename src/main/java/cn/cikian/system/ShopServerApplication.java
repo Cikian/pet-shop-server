5d8805271f3874b42276e29e2bca93b4eaabad88
Cikian_Y9000P <cikian@126.com>
2026-04-15 22:58:34 +0800

    2

--- src/main/java/cn/cikian/system/ShopServerApplication.java
+++ src/main/java/cn/cikian/system/ShopServerApplication.java
@@ -1,5 +1,7 @@
 package cn.cikian.system;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ShopServerApplication {
 
     public static void main(String[] args) {
+        // 加载 .env 文件
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // 将 .env 中的每一项放入系统属性，以便 Spring Boot 的 ${...} 可以读取
+        for (DotenvEntry entry : dotenv.entries()) {
+            System.out.println("环境变量["+ entry.getKey() + " = " +  entry.getValue() + "]");
+            System.setProperty(entry.getKey(), entry.getValue());
+        }
+
         SpringApplication.run(ShopServerApplication.class, args);
 
         log.info("启动成功...");
